b5a4963fb60199266303e91834f5315b45c677c2
Srinivas <srinu898it@gmail.com>
2024-05-15 22:17:57 -0400

    Added Private subnet, SG, Ec2, DB resources

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "retail_vpc" {
   cidr = var.cidr
   enable_dns_hostnames = true
   tags = {
-     Name = "AWS VPC for Retail NorthAmerica"
+     Name = "AWS VPC for Walmart Retail NorthAmerica"
   } 
 }
 
@@ -35,6 +35,20 @@ resource "aws_subnet" "retail_public" {
   }
 }
 
+#-----------------------
+# Create Private Subnet
+#-----------------------
+
+resource "aws_subnet" "retail_private" {
+    vpc_id = aws_vpc.retail_vpc.id
+    cidr == var.subnet2_cidr
+    availability_zone = data.aws_availability_zones.all.names[1]
+    tags = {
+        name = "Private subnet for Retail"
+    }
+}
+
+
 #------------------------
 # Create Internet gateway
 #------------------------
@@ -48,10 +62,10 @@ resource "aws_internet_gateway" "retail_igw" {
 
 
 #-------------------------------
-# Create Route table and Routes
+# Create Public Route table
 #-------------------------------
 
-resource "aws_route_table" "retail_route_1" {
+resource "aws_route_table" "retail_public_route" {
    vpc_id = aws_vpc.retail_vpc.id
 
    route = {
@@ -64,16 +78,41 @@ resource "aws_route_table" "retail_route_1" {
 }
 
 
-#----------------------------------
-# Assoicaite RouteTable with Subnet
-#----------------------------------
+#---------------------------
+# Create private Route Table
+#---------------------------
+
+resource "aws_route_table" "retail_private_route" {
+     vpc_id = aws_vpc.retail_vpc.id
+     route = {
+     cidr_block = "0.0.0.0/0"
+     instance_id = aws_instance.uatServer.id
+     }
+}
+
+
+#-----------------------------------------
+# Assoicaite Public RouteTable with Subnet
+#-----------------------------------------
 
 
 resource "aws_route_table_assoiciation" "retail_east1_public" {
      subnet_id = aws_subnet.retail_public.id
-     route_table_id = aws_route_table_retail_route_1.id
+     route_table_id = aws_route_table_retail_public_route.id
 
 }
+
+
+#-----------------------------------------
+# Assoicate Private Routetable with Subnet
+#-----------------------------------------
+
+resource "aws_route_table_assoication" "retail_east1_private" {
+    subnet_id = aws_subnet.retail_private.id
+    route_table_id = aws_route_table_retail_private_route.id
+}
+
+
 
 
 #-----------------------
@@ -106,6 +145,28 @@ resource "aws_security_group" "retail_sg" {
    }
 }
 
+#--------------------------
+# Create SG for DB Server
+#--------------------------
+
+resource "aws_security_group" "retail_sg_db" {
+    name = "retail_database_secgroup"
+    egress {
+     from_port = 0
+     to_port = 0
+     protocal = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+      from_port = 3306
+      to_port = 3306
+      protocal = "tcp"
+      security_groups = [aws_security_group.retail_sg.id]
+
+}
+
+
+
 resource "aws_instance" "devServer" {
     ami = var.amiid
     instance_type = var.type
@@ -125,4 +186,23 @@ resource "aws_instance" "devServer" {
     }
 }
 
+resource "aws_instance" "dbServer" {
+   ami = var.amiid
+   instance_type = var.type
+   key_name = var.pemfile
+   vpc_security_group_ids = [aws_security_group.retail_sg_db.id]
+   subnet_id = aws_subnet.retail_private.id
+   availability_zone = data.aws_availability_zone.all.names[1]
+
+   associate_public_ip_address = true
+
+   user_data = <<-EOF
+   		 #!/bin/bash
+		 touch /tmp/db_creation.txt
+		 EOF
+
+   tags = {
+     name = "Retail DB Server"
+   }
+}
 
