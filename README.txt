c4d85f095bf0206d765ada72f8050cfe57805814
Srinivas <srinu898it@gmail.com>
2024-05-15 21:46:23 -0400

    Adding Readme file. Updated Main file with Ec2, SG

---
+++ README.txt
@@ -0,0 +1,20 @@
+#########################################################
+Author : Srinivasreddy Sangam                          ##
+Description: Creating AWS Resources using Terraform    ##
+Date : May 15, 2024                                    ##
+Last Updated : May 15, 2024                            ##
+#########################################################
+
+Creating AWS Infrastructure
+
+1. Create VPC
+2. Create Public Subnet & Private subnet
+3. Create Security Groups
+4. Create Internet Gateway
+5. Create NAT Gateway
+5. Create Route Table
+6. Create IAM user
+7. Create EC2 server
+8. Create S3 bucket
+
+
--- main.tf
+++ main.tf
@@ -35,18 +35,94 @@ resource "aws_subnet" "retail_public" {
   }
 }
 
+#------------------------
+# Create Internet gateway
+#------------------------
+
 resource "aws_internet_gateway" "retail_igw" {
-   vpc_id = aws_vpc.aws.id
+   vpc_id = aws_vpc.retail_vpc.id
    tags = {
     name = "Retail InternetGateway"
    }
 }
 
 
+#-------------------------------
+# Create Route table and Routes
+#-------------------------------
+
+resource "aws_route_table" "retail_route_1" {
+   vpc_id = aws_vpc.retail_vpc.id
+
+   route = {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.retail_igw.id
+   }
+   tags = {
+      name = "Public route through IGW"
+   }
+}
 
 
+#----------------------------------
+# Assoicaite RouteTable with Subnet
+#----------------------------------
 
 
+resource "aws_route_table_assoiciation" "retail_east1_public" {
+     subnet_id = aws_subnet.retail_public.id
+     route_table_id = aws_route_table_retail_route_1.id
 
+}
+
+
+#-----------------------
+# Create security Group
+#-----------------------
+
+
+resource "aws_security_group" "retail_sg" {
+
+   name = "retail_sg_1"
+   vpc_id = aws_vpc.retail_vpc.id
+
+   #Outbound Rule
+
+   egress {
+
+     from_port = 0
+     to_port = 0
+     protocal = -1
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   #Inbound Rule
+   ingress {
+   
+     from_port = 22
+     to_port = 22
+     protocal = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+}
+
+resource "aws_instance" "devServer" {
+    ami = var.amiid
+    instance_type = var.type
+    key_name = var.pemfile
+    vpc_security_group_ids = [aws_security_group.retail_sg.id]
+    subnet_id = aws_subnet.retail_public.id
+    availability_zone = data.aws_availability_zone.all.names[0]
+ 
+    assoiciate_public_ip_address = true
+
+    user_data = <<-EOF
+                   #!/bin/bash
+		   touch /tmp/instance_creation.txt
+		   EOF
+    tags = {
+       name = "Retail Dev Server"
+    }
+}
 
 
