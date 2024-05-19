a151398ab7ece5e0190d76b442be67bce56d9b07
Srinivas <srinu898it@gmail.com>
2024-05-18 20:28:24 -0400

    Creating ECR repositaries

--- main.tf
+++ main.tf
@@ -1,14 +1,31 @@
 provider "aws" {
-   region = "ap-south-1"
+   region = "us-east-1"
 }
 
-#Configuration of EC2 instance
-resource "aws_instance" "demo_server" {
- ami = "ami-id"
- instance_type = "t2.micro"
+
+# Create Multiple docker repos
+
+variable "repo_names" {
+
+  type = list
+  default = ["webapp1", "webapp2"]
 }
 
-#Configuration of S3 bucket
-resource "aws_s3_bucket" "demo_bucket" {
-   bucket = "Sample Bucket"
+
+#Create private repositary for each application
+
+resource "aws_ecr_repositary" {
+
+  count = length(var.repo_names)
+  name = var.repo_names[count.index]
+  image_tag_mutability = "MUTABLE"
+
+  image_scaning_configuration {
+   scan_on_push = true
+  }
 }
+
+
+
+
+
