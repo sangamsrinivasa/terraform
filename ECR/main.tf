ae4a2d3c5d27a560220cf8e467a1ac52c115564c
Srinivas <srinu898it@gmail.com>
2024-06-06 08:37:53 -0400

    Added Webapp repositaries

--- ECR/main.tf
+++ ECR/main.tf
@@ -11,12 +11,31 @@ variable "app_environment" {
   default = "dev"
 }
 
+variable "repo_names" {
+
+  type = list
+  default = ["webapp1", "webapp2"]
+}
+
 # ecr.tf | Elastic Container Repository
 
-resource "aws_ecr_repository" "aws-ecr" {
+resource "aws_ecr_repository" "banking-ecr" {
   name = "${var.app_name}-${var.app_environment}-ecr"
   tags = {
     Name        = "${var.app_name}-ecr"
     Environment = var.app_environment
+  }
+}
+
+#Create private repositary for each application
+
+resource "aws_ecr_repositary" "webapp-ecr" {
+  count = length(var.repo_names)
+  name = var.repo_names[count.index]
+  #Allows tag over-writing
+  image_tag_mutability = "MUTABLE"
+
+  image_scaning_configuration {
+   scan_on_push = true
   }
 }
