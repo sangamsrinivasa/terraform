080b9f0fd9e0b0ed4e4da0095be5447ba035fa5a
Srinivas <srinu898it@gmail.com>
2024-06-21 00:13:56 -0400

    Creating Kafka Cluster with broker nodes

---
+++ MKS/main.tf
@@ -0,0 +1,86 @@
+provider "aws" {
+  region = "us-west-2"  # Change to your desired region
+}
+
+resource "aws_vpc" "asia_stream" {
+  cidr_block = var.vpc-cidr
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.asia_stream.id
+  cidr_block = var.subnet-cidr-1
+  availability_zone = "us-west-2a"  # Change to your desired AZ
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.asia_stream.id
+  cidr_block = var.subnet-cidr-2
+  availability_zone = "us-west-2b"  # Change to your desired AZ
+}
+
+resource "aws_subnet" "subnet3" {
+  vpc_id     = aws_vpc.asia_stream.id
+  cidr_block = var.subnet-cidr-3
+  availability_zone = "us-west-2c"  # Change to your desired AZ
+}
+
+resource "aws_security_group" "kafka_sg" {
+  vpc_id = aws_vpc.asia_stream.id
+
+  ingress {
+    from_port   = 9092
+    to_port     = 9092
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # open inbound rule, can be confied.
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_msk_cluster" "kafka_cluster" {
+  cluster_name           = var.kafka-cluster-name
+  kafka_version          = "2.8.1"
+  number_of_broker_nodes = 3
+
+  broker_node_group_info {
+    instance_type = "kafka.m5.large"
+    ebs_volume_size = 100
+    client_subnets = [
+      aws_subnet.subnet1.id,
+      aws_subnet.subnet2.id,
+      aws_subnet.subnet3.id,
+    ]
+    security_groups = [aws_security_group.kafka_sg.id]
+  }
+
+  encryption_info {
+    encryption_in_transit {
+      client_broker = "TLS"
+      in_cluster    = true
+    }
+  }
+
+  configuration_info {
+    arn      = aws_msk_configuration.kafka_configuration.arn
+    revision = aws_msk_configuration.kafka_configuration.latest_revision
+  }
+
+  tags = {
+    Name = "kafka-cluster"
+  }
+}
+
+resource "aws_msk_configuration" "kafka_configuration" {
+  name = "example-kafka-configuration"
+  kafka_versions = ["2.8.1"]
+  server_properties = <<PROPERTIES
+auto.create.topics.enable = true
+delete.topic.enable = true
+PROPERTIES
+}
+
---
+++ MKS/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc-cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet-cidr-1" {
+   default = "10.0.2.0/24"
+}
+
+variable "subnet-cidr-2" {
+   default = "10.0.3.0/24"
+}
+
+variable "subnet-cidr-3" {
+  default = "10.0.4.0/24"
+}
+
+
+variable "kafka-cluster-name" {
+  default = "asia_stream_dev" 
+}
+
+
+
+
+
+
+
+
+
